161c75e417d56a265bef7f00c5a8a66dbc35f166
Tiago Pace <tiagopace.dev@gmail.com>
2024-07-09 15:04:05 -0300

    Initial project setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/test', () => {
+  return 'Hello'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('listening on portt')
+})
