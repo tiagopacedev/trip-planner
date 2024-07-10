492f194b40b613346636cefaec544b0991cbf157
Tiago Pace <tiagopace.dev@gmail.com>
2024-07-09 23:59:59 -0300

    feat: add participant confirmation endpoint

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from '@fastify/cors'
 import { createTrip } from './routes/create-trip'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { confirmTrip } from './routes/confirm-trip'
+import { confirmParticipants } from './routes/confirm-participant'
 
 const app = fastify()
 
@@ -16,6 +17,7 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipants)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('listening on 3333')
