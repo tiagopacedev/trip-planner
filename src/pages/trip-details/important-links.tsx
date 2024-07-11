20db4d88d0648ebc3251e0d554047abe7e06887f
Tiago Pace <tiagopace.dev@gmail.com>
2024-07-10 23:34:15 -0300

    feat(ImportantLinks): list important links

--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,43 +1,58 @@
 import { Link2, Plus } from "lucide-react"
 import { Button } from "../../components/button"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { api } from "@/lib/axios"
+
+interface Link {
+  id: string
+  title: string
+  url: string
+}
 
 export function ImportantLinks() {
+  const { tripId } = useParams()
+  const [links, setLinks] = useState<Link[]>([])
+
+  useEffect(() => {
+    api
+      .get(`trips/${tripId}/links`)
+      .then((response) => setLinks(response.data.links))
+  }, [tripId])
+
+  const copyToClipboard = (url: string) => {
+    navigator.clipboard.writeText(url)
+  }
+
   return (
     <div className="space-y-6">
       <h2 className="text-xl font-semibold">Links importantes</h2>
 
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva do AirBnB
-            </span>
-            <a
-              href="#"
-              className="block truncate text-xs text-zinc-400 hover:text-zinc-200"
-            >
-              https://www.airbnb.com.br/rooms/10470001139028321098312093821903812038910
-            </a>
+        {links.map((link) => (
+          <div
+            key={link.id}
+            className="flex items-center justify-between gap-4"
+          >
+            <div className="space-y-1.5">
+              <span className="block font-medium text-zinc-100">
+                Reserva do AirBnB
+                {link.title ?? "Link"}
+              </span>
+              <a
+                href={link.url}
+                className="block truncate text-xs text-zinc-400 hover:text-zinc-200"
+              >
+                {link.url}
+              </a>
+            </div>
+
+            <Link2
+              className="size-5 shrink-0 text-zinc-400"
+              onClick={() => copyToClipboard(link.url)}
+            />
           </div>
-
-          <Link2 className="size-5 shrink-0 text-zinc-400" />
-        </div>
-
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva do AirBnB
-            </span>
-            <a
-              href="#"
-              className="block truncate text-xs text-zinc-400 hover:text-zinc-200"
-            >
-              https://www.airbnb.com.br/rooms/10470001139028321098312093821903812038910
-            </a>
-          </div>
-
-          <Link2 className="size-5 shrink-0 text-zinc-400" />
-        </div>
+        ))}
       </div>
 
       <Button variant="secondary" size="full">
